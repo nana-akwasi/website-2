61ce8b5dd049546dc7f7b02adaf600f86f8550a9
nana-akwasi <nana505abgmail.com>
2022-05-28 10:53:17 -0500

    fix bug

--- Resources/vpc/subnet.tf
+++ Resources/vpc/subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "subnet-1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet_cidr_1
+  availability_zone = "us-east-1"
 
   tags = {
     Name = var.project_name_1
@@ -10,6 +11,7 @@ resource "aws_subnet" "subnet-1" {
 resource "aws_subnet" "subnet-2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet_cidr_2
+  availability_zone = "us-east-2"
 
   tags = {
     Name = var.project_name_2
